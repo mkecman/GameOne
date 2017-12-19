c846b119661e6488d265e2ab03140eae14c7f27d
Marko Kecman <mkecman@gmail.com>
2017-12-19 17:42:07 +0100

    Started adding views;

---
+++ Assets/Scripts/Life/LifeView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LifeView : MonoBehaviour
+{
+    public Transform ElementPanel;
+    public GameObject ElementPrefab;
+    public PropertyView Population;
+    public PropertyView Science;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        LifeModel _life = getModel<LifeModel>();
+    }
+
+    private T getModel<T>()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Property/PropertyView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropertyView : MonoBehaviour
+{
+    public string Property;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
