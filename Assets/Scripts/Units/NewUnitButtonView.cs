c64ac30b39bbc093c0de29aa0661609967fd9ca7
Marko Kecman <mkecman@gmail.com>
2018-06-05 23:22:45 +0200

    Refactored NewUnitButtonView to show more unit's properties; Reorganized Prefabs Folder; Added ResistanceGraphStatic for simple showing of BellCurve values;

--- Assets/Scripts/Units/NewUnitButtonView.cs
+++ Assets/Scripts/Units/NewUnitButtonView.cs
@@ -1,65 +1,80 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
+﻿using System;
 using TMPro;
-using System;
-using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class NewUnitButtonView : GameView
 {
     public int UnitTypeIndex;
     public Button Button;
-    public TextMeshProUGUI Effect1;
-    public TextMeshProUGUI Effect2;
-    private UnitTypesConfig _unitConfig;
+
+    public TextMeshProUGUI Name;
+    public UIPropertyView Effect1;
+    public UIPropertyView Effect2;
+
+    public UIPropertyView Body;
+    public UIPropertyView Mind;
+    public UIPropertyView Soul;
+
+    public ResistanceGraphStatic ResistanceTemperature;
+    public ResistanceGraphStatic ResistancePressure;
+    public ResistanceGraphStatic ResistanceHumidity;
+    public ResistanceGraphStatic ResistanceRadiation;
+
+
+    private UnitTypesConfig _unitTypesConfig;
+    private UnitTypeData _unit;
     private int _X;
     private int _Y;
 
     void Start()
     {
-        _unitConfig = GameConfig.Get<UnitTypesConfig>();
+        _unitTypesConfig = GameConfig.Get<UnitTypesConfig>();
+        _unit = _unitTypesConfig[ UnitTypeIndex ];
         GameModel.HandleGet<HexModel>( OnHexModelChange );
 
         Button.onClick.AddListener( OnButtonClick );
 
-        //Extremely hacky way to show two values
-        //TODO: Refactor this!!!
-        bool isFirst = true;
-        if( _unitConfig[ UnitTypeIndex ].ITemperature != 0 )
+        Name.text = _unit.Name;
+        Body.SetValue( _unit.Body );
+        Mind.SetValue( _unit.Mind );
+        Soul.SetValue( _unit.Soul );
+
+        ResistanceTemperature.SetPosition( _unit.Temperature );
+        ResistancePressure.SetPosition( _unit.Pressure );
+        ResistanceHumidity.SetPosition( _unit.Humidity );
+        ResistanceRadiation.SetPosition( _unit.Radiation );
+
+        UIPropertyView currentEffect = Effect1;
+        if( _unit.ITemperature != 0 )
         {
-            Effect1.text = "TMP " + _unitConfig[ UnitTypeIndex ].ITemperature.ToString();
-            isFirst = false;
+            SetEffectValue( currentEffect, R.Temperature );
+            currentEffect = Effect2;
         }
-        if( _unitConfig[ UnitTypeIndex ].IPressure != 0 )
+        if( _unit.IPressure != 0 )
         {
-            if( isFirst )
-            {
-                Effect1.text = "PRS " + _unitConfig[ UnitTypeIndex ].IPressure.ToString();
-                isFirst = false;
-            }
-            else
-                Effect2.text = "PRS " + _unitConfig[ UnitTypeIndex ].IPressure.ToString();
+            SetEffectValue( currentEffect, R.Pressure );
+            currentEffect = Effect2;
         }
-        if( _unitConfig[ UnitTypeIndex ].IHumidity != 0 )
+        if( _unit.IHumidity != 0 )
         {
-            if( isFirst )
-            {
-                Effect1.text = "HUM " + _unitConfig[ UnitTypeIndex ].IHumidity.ToString();
-                isFirst = false;
-            }
-            else
-                Effect2.text = "HUM " + _unitConfig[ UnitTypeIndex ].IHumidity.ToString();
+            SetEffectValue( currentEffect, R.Humidity );
+            currentEffect = Effect2;
         }
-        if( _unitConfig[ UnitTypeIndex ].IRadiation != 0 )
+        if( _unit.IRadiation != 0 )
         {
-            if( isFirst )
-            {
-                Effect1.text = "RAD " + _unitConfig[ UnitTypeIndex ].IRadiation.ToString();
-                isFirst = false;
-            }
-            else
-                Effect2.text = "RAD " + _unitConfig[ UnitTypeIndex ].IRadiation.ToString();
+            SetEffectValue( currentEffect, R.Radiation );
         }
+    }
+
+    private void SetEffectValue( UIPropertyView Effect, R type )
+    {
+        Effect.SetProperty( type.ToString() );
+        Effect.SetValue( Convert.ToSingle( GetUnitImpactValue( _unit, type ) ) );
+    }
+
+    private object GetUnitImpactValue( object src, R type )
+    {
+        return src.GetType().GetField( "I"+type.ToString() ).GetValue( src );
     }
 
     private void OnHexModelChange( HexModel value )
