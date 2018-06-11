6f10eaf14362ac4124767348707e13e68cbce967
Marko Kecman <mkecman@gmail.com>
2018-06-11 21:54:10 +0200

    Changed BellCurve to use linear function;

--- Assets/Scripts/BellCurve/BellCurve.cs
+++ Assets/Scripts/BellCurve/BellCurve.cs
@@ -48,12 +48,8 @@ public class BellCurve
 
     public float GetFloatAt( float time )
     {
-        return Amplitude.Value * Mathf.Exp( -Mathf.Pow( time - Position.Value, 2 ) / Range.Value );
-    }
-
-    public int GetIntAt( float time )
-    {
-        return (int)( Amplitude.Value * Mathf.Exp( -Mathf.Pow( time - Position.Value, 2 ) / Range.Value ) * 100 );
+        return 1f - Math.Abs( time - Position.Value ) * 2f;
+        //return Amplitude.Value * Mathf.Exp( -Mathf.Pow( time - Position.Value, 2 ) / Range.Value );
     }
 
     public bool ChangePosition( float delta )
